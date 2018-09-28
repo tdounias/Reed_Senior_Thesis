d39abb2feb0ba4e06f0f8d6bbc489d5efd416209
tdounias <tdounias@reed.edu>
2018-09-28 15:49:30 -0700

    finalized turnout function

--- data_wrangling/votes_by_year_functionalized.R
+++ data_wrangling/votes_by_year_functionalized.R
@@ -8,6 +8,10 @@ turnout_calc <- function(regfile, histfile, el_date, county){
   
   ##First the denominator
   
+  if(typeof(regfile$REGISTRATION_DATE) != "double") regfile$REGISTRATION_DATE <- mdy(regfile$REGISTRATION_DATE)
+  
+  if(typeof(histfile$ELECTION_DATE) != "double") histfile$ELECTION_DATE <- mdy(histfile$ELECTION_DATE)
+  
   #Filter for relevant year, registrants
   regfile_current <- regfile %>%
     filter(FILE_YEAR == year(el_date)) %>%
@@ -23,7 +27,7 @@ turnout_calc <- function(regfile, histfile, el_date, county){
   ##Then the numerator
   
   #Take relevant votes
-  nums <- vhist_full %>%
+  nums <- histfile %>%
     filter(ELECTION_DATE == el_date)
   
   #Filter out those with no voter registration
@@ -33,10 +37,9 @@ turnout_calc <- function(regfile, histfile, el_date, county){
     group_by(COUNTY_NAME) %>%
     summarise(votes = sum(n()))
   
+  names(nums)[1] <- "county"
+  
   #Now for a final step, to calculate turnout
-  turnouts <- merge(nums, denoms, by = county) %>%
-    rename(county = COUNTY_NAME) %>%
+  turnouts <- merge(nums, denoms, by = "county") %>%
     mutate(turnout = votes/reg)
 }
-
-
