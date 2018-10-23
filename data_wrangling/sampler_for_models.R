266c8e51dd11b83a292cac5213c431c12422a8a1
tdounias <tdounias@reed.edu>
2018-10-23 16:55:28 -0700

    model checks

---
+++ data_wrangling/sampler_for_models.R
@@ -0,0 +1,97 @@
+library(tidyverse)
+library(lubridate)
+library(knitr)
+source("~/Desktop/Reed_Senior_Thesis/riggd/R/utils.R")
+setwd("~/Desktop/Reed_Senior_Thesis/Data_and_results/data")
+
+#Load the data
+load("indiv_model_list.RData")
+
+#Make df from list
+model_dt <- extract_indiv_model_data(indiv_voter_list)
+
+set.seed(14)
+dt_frac <- model_dt %>%
+  group_by(COUNTY, MAIL_VOTE, voted) %>%
+  summarise(n()/nrow(model_dt))
+
+model_sample <- model_dt %>%
+  group_by(MAIL_VOTE, voted, COUNTY) %>%
+  mutate(num_rows=n()) %>%
+  sample_frac(0.01, weight=num_rows) %>%
+  ungroup
+
+#Check fraction of difference
+dt_frac_after <- model_sample %>%
+  group_by(COUNTY, MAIL_VOTE, voted) %>%
+  summarise(n()/nrow(model_sample))
+
+diffs <- (dt_frac[,4] - dt_frac_after[,4])
+
+#The absolute value of differences has a mean of about .1%
+mean(abs(diffs[,1]))
+
+#And a standard deviation of .3%
+sd(abs(diffs[,1]))
+
+#Check all variables
+##Voter ID
+length(unique(model_sample$VOTER_ID))
+
+sum(is.na(model_sample$VOTER_ID))
+
+
+##County
+length(unique(model_sample$COUNTY))
+
+sum(is.na(model_sample$COUNTY))
+
+
+##Party
+unique(model_sample$PARTY)
+
+sum(is.na(model_sample$PARTY))
+
+
+##Gender
+unique(model_sample$GENDER)
+
+sum(is.na(model_sample$GENDER))
+
+summary(as.factor(model_sample$GENDER))
+
+
+##Age
+unique(model_sample$AGE)
+
+sum(is.na(model_sample$AGE))
+
+
+##Election Type
+unique(model_sample$ELECTION_TYPE)
+
+sum(is.na(model_sample$ELECTION_TYPE))
+
+
+##Election Date
+unique(model_sample$ELECTION_DATE)
+
+sum(is.na(model_sample$ELECTION_DATE))
+
+summary(as.factor(model_sample$ELECTION_DATE))
+
+
+##Mail Vote
+unique(model_sample$MAIL_VOTE)
+
+sum(is.na(model_sample$MAIL_VOTE))
+
+summary(as.factor(model_sample$MAIL_VOTE))
+
+
+##Voted and Mail Vote
+sum(model_sample$voted == model_sample$MAIL_VOTE)/nrow(model_sample)
+
+model_sample <- na.omit(model_sample)
+
+write.csv(model_sample, "model_indiv_sample.csv")
---
+++ models/county_model_first_draft.R
@@ -0,0 +1,57 @@
+library(tidyverse)
+library(lubridate)
+library(Matrix)
+library(lme4)
+source("~/Desktop/Reed_Senior_Thesis/riggd/R/utils.R")
+setwd("~/Desktop/Reed_Senior_Thesis/Data_and_results/data")
+
+model_sample <- read_csv("model_indiv_sample.csv")
+demographics <- read_csv("colorado_demographic_stats_by_county.csv")
+
+model_sample <- left_join(model_sample, demographics, by = "COUNTY")
+
+#Run the model
+md_1 <- glmer(family = "binomial", data = model_sample, 
+              voted ~ (1|COUNTY) + PCT_URBAN + PCT_WHITE)
+
+#Display results
+arm::display(md_1)
+
+#Fixed effects
+fixef(md_1)
+
+#########################
+##Diagnostics and Plots##
+#########################
+
+##12.6 Graphs
+county_coefs <- coef(md_1)[[1]][,1]
+
+plot_data <- data.frame(cbind(county_coefs, demographics$PCT_URBAN, demographics$PCT_WHITE), ranef(md_1)[[1]][,1])
+
+names(plot_data) <- c("estimated_coefs", "urban", "white", "coef_se")
+
+##Urban pop graph (12.6)
+ggplot(plot_data, aes(x = urban, y = estimated_coefs)) +
+  geom_pointrange(aes(ymin= estimated_coefs-coef_se, ymax=estimated_coefs+coef_se), size = .1) +
+  geom_abline(slope = fixef(md_1)[2], intercept = fixef(md_1)[1])
+
+##White pop graph (12.6)
+ggplot(plot_data, aes(x = white, y = estimated_coefs)) +
+  geom_pointrange(aes(ymin= estimated_coefs-coef_se, ymax=estimated_coefs+coef_se), size = .1) +
+  geom_abline(slope = fixef(md_1)[3], intercept = fixef(md_1)[1])
+
+
+##12.5 Graphs
+#Put counties into plot_data
+# county <- demographics[,2]
+# plot_data <- cbind(plot_data, county)
+# 
+# plot_data2 <- plot_data %>%
+#   filter(COUNTY %in% c("Arapahoe", "Denver", "Summit", "Yuma", "Weld", 
+#                        "Boulder", "Bent", "Custer")) %>%
+  
+
+ggplot(demographics, aes(x = PCT_URBAN, y = PCT_WHITE, col = PCT_OF_STATE_POP)) +
+  geom_jitter()
+  
