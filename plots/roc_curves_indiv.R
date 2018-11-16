0b71966c89bb27e82148cb9f991d8858818a588b
tdounias <tdounias@reed.edu>
2018-11-15 19:00:50 -0800

    suggested chpt4 changes county-lvl

--- plots/roc_curves_indiv.R
+++ plots/roc_curves_indiv.R
@@ -4,10 +4,16 @@ library(Matrix)
 library(lme4)
 library(pROC)
 library(purrr)
+library(gamm4)
+library(gam)
 source("~/Desktop/Reed_Senior_Thesis/riggd/R/utils.R")
 setwd("~/Desktop/Reed_Senior_Thesis/Data_and_results/data")
 
 load("indiv_model_list.RData")
+
+model_sample <- read_csv("model_indiv_sample.csv")
+
+model_sample$AGE <- scale(model_sample$AGE)
 
 rocs <- map(model_list, ~get_rocs(.x, model_sample))
 
@@ -29,5 +35,9 @@ ggplot(graph_data, aes(x = Sensitivity, y = Specificity, col = Model)) +
   geom_line() +
   geom_abline(intercept = 0, slope = 1, alpha = .4, linetype = "twodash") +
   xlab("1 - Sensitivity") +
-  scale_color_brewer(palette = "Dark2")
+  scale_color_brewer(palette = 1) +
+  theme_bw() +
+  annotate("text", x = .65, y = .75, label = "1, 2, 3a") +
+  annotate("text", x = .35, y = .75, label = "4") +
+  annotate("text", x = .07, y = .75, label = "5, 5a")
  
--- plots/vbm_county_graph.R
+++ plots/vbm_county_graph.R
@@ -13,10 +13,34 @@ graph_data <- turnouts_county_data(turnout_list) %>%
 
 names(graph_data) <- c("year", "pct_vbm")
 
+before_2010 <- read_csv("2012hist.csv")
+
+before_2010$ELECTION_DATE <- year(mdy(before_2010$ELECTION_DATE))
+
+mail_vote <- c("Absentee Carry", "Absentee Mail", "Mail Ballot", 
+               "Mail Ballot - DRE")
+
+before_2010 <- before_2010 %>%
+  filter(ELECTION_DATE %in% c(2002:2009)) %>%
+  filter(ELECTION_TYPE %in% c("Primary", "General", "Coordinated")) %>%
+  mutate(vbm = (VOTING_METHOD %in% mail_vote)) %>%
+  group_by(ELECTION_DATE) %>%
+  summarise(pct_vbm = sum(vbm)/n())
+
+names(before_2010)[1] <- "year"
+
+graph_data <- rbind(graph_data, before_2010)
+
 ggplot(graph_data, aes(x = year, y = pct_vbm)) +
-  geom_vline(xintercept = 2013, col = "red") +
-  geom_hline(yintercept = 1) +
+  geom_vline(xintercept = 2013, col = "grey", lty = 2) +
+  geom_vline(xintercept = 2008, col = "grey", lty = 2) +
   geom_point(size = .2) +
+  theme_bw() +
   geom_line(alpha = .7, col = "dark blue") +
   labs(x = "Election Year", y = "% of Mail Ballots Over Total Ballots") +
-  guides(col=FALSE)
+  guides(col=FALSE) +
+  ylim(c(0, 1)) +
+  geom_rect(xmin =2010,xmax = 2016, ymin = .75, ymax =1, fill = 10, alpha = .01) +
+  annotate("text", x = 2013, y = .70, label = "Range of Trustworthy Data") +
+  annotate("text", x = 2008, y = .1, col = "dark grey", label = "2008 Reform") +
+  annotate("text", x = 2013, y = .1, col = "dark grey", label = "2013 Reform")
