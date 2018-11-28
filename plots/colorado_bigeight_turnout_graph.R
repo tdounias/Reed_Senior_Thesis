6334de7fb8cae307664758b82efe17dc404b6fd7
tdounias <tdounias@reed.edu>
2018-11-27 19:24:55 -0800

    fixed post-sub comma

--- plots/colorado_bigeight_turnout_graph.R
+++ plots/colorado_bigeight_turnout_graph.R
@@ -15,7 +15,7 @@ ggplot(graph_data, aes(x = dates, y = turnout, col = Type)) +
   geom_vline(xintercept = 2013, col = "grey", lty = 2) +
   geom_point() +
   annotate("text", x = 2013, y = .2, label = "2013 Reform", size = 3, col = "grey") +
-  scale_color_manual(values = c("#00ccff", "#000066", "#0066ff", "#0033ff")) +
+  scale_color_manual(values = c("#00ccff", "#000066", "#00ffff", "#0033ff")) +
   theme_bw() +
   labs(x = "Election Year", y = "Turnout as % of Registered Voters") +
   ggsave("colorado_bigeight_turnout_graph.png", width = 5.00, height = 4.20)
